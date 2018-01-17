83b765473709877174dc6eec456f65de6d37b60e
ccnuyan <ccnuyan@gmail.com>
2018-01-17 09:35:54 +0800

    try after night

--- database/sql/1-0-0/05-functions/locate_user_by_password.sql
+++ database/sql/1-0-0/05-functions/locate_user_by_password.sql
@@ -3,10 +3,10 @@ set search_path = starcedu_auth;
 create or replace function locate_user_by_password(username varchar, pass varchar)
 returns bigint
 as $$
-  SET search_path=starcedu_auth;
+  SET search_path=starcedu_auth, public;
 
   select user_id from logins where
   provider_key = username and
-  provider_token = crypt(pass::text, provider_token::text);
+  provider_token = public.crypt(pass::text, provider_token::text);
 $$
 language sql;
--- database/sql/1-0-0/05-functions/update_password.sql
+++ database/sql/1-0-0/05-functions/update_password.sql
@@ -10,12 +10,12 @@ DECLARE
   return_login_info starcedu_auth.login_info;
 BEGIN
   set search_path=starcedu_auth, public;
-  
+
   --find the user based on username/password
   select locate_user_by_password(input_username, old_pass) into found_id;
   IF (found_id IS not NULL) THEN
     --update the password if all is OK
-    update logins set provider_token = crypt(new_pass::text, gen_salt('bf',10)::text)
+    update logins set provider_token = public.crypt(new_pass::text, public.gen_salt('bf',10)::text)
     where user_id=found_id and provider='local';
   ELSE
     success := FALSE;
